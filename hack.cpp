b8592334df8eaf038d53433e3101f4016a0ef203
aimanalrafi <80934568+aimanalrafi@users.noreply.github.com>
2023-07-29 09:02:52 +0200

    adjusted sleep time

--- hack.cpp
+++ hack.cpp
@@ -154,7 +154,7 @@ int main() {
     // Monitor the region continuously
     while (true) {
         monitorRegion();
-        std::this_thread::sleep_for(std::chrono::milliseconds(250)); 
+        std::this_thread::sleep_for(std::chrono::milliseconds(100)); 
     }
 
     return 0;
